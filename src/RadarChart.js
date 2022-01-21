145e5fbd61234288de7658932899ce78b69a790f
Th1nhNg0 <ngophuthinh2001@gmail.com>
2022-01-21 19:58:45 +0700

    update

--- src/RadarChart.js
+++ src/RadarChart.js
@@ -1,32 +1,35 @@
 import React, { useState } from "react";
 
 export default function RadarChart() {
-  const [type, settype] = useState("c1");
+  const [League, setLeague] = useState("c1");
+  const [type, settype] = useState("def");
   return (
     <div className="py-5">
-      <div className="w-full">
-        <select
-          className="block p-3 mx-auto border-2 border-yellow-400 rounded "
-          onChange={(e) => settype(e.target.value)}
-        >
-          <option value="c1">Champions League</option>
-          <option value="dc">Domestic League</option>
-        </select>
+      <div className="flex items-center justify-center w-full gap-5">
+        <div>
+          <select
+            className="block p-3 mx-auto border-2 border-yellow-400 rounded "
+            onChange={(e) => setLeague(e.target.value)}
+          >
+            <option value="c1">Champions League</option>
+            <option value="dc">Domestic League</option>
+          </select>
+        </div>
+        <div>
+          <select
+            className="block p-3 mx-auto border-2 border-yellow-400 rounded "
+            onChange={(e) => settype(e.target.value)}
+          >
+            <option value="def">Defensive</option>
+            <option value="off">Offensive </option>
+            <option value="pass">Passing </option>
+          </select>
+        </div>
       </div>
       <iframe
         className="w-full h-[500px]"
         title="sotranthidau"
-        src={`graph/radar/def${type}.html`}
-      ></iframe>
-      <iframe
-        className="w-full h-[500px]"
-        title="sotranthidau"
-        src={`graph/radar/off${type}.html`}
-      ></iframe>
-      <iframe
-        className="w-full h-[500px]"
-        title="sotranthidau"
-        src={`graph/radar/pass${type}.html`}
+        src={`graph/radar/${type}${League}.html`}
       ></iframe>
     </div>
   );
