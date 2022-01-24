2fe789ff77e2ddfad3ee1e3305f36cb37d6e2389
Th1nhNg0 <ngophuthinh2001@gmail.com>
2022-01-24 10:12:39 +0700

    update

--- src/Achievements.js
+++ src/Achievements.js
@@ -58,18 +58,32 @@ export default function Achievements() {
         >
           Title
         </button>
+        <button
+          onClick={() => settype("Record")}
+          className={`px-5 py-3 text-white ${
+            type === "Record" ? "bg-yellow-500" : "bg-gray-500"
+          } rounded`}
+        >
+          Record
+        </button>
       </div>
-      <div
-        className={`${
-          showMore ? "max-h-auto" : "max-h-[50vh]"
-        } overflow-hidden space-y-5`}
-      >
-        {awardsName.map((award) => (
-          <div key={award}>
-            <p className="font-semibold text-center">{award}</p>
-            <div className="flex">
-              <div
-                className={`flex-1 p-3  bg-gradient-to-l
+      {type !== "Record" ? (
+        <div>
+          <div className="flex justify-evenly">
+            <h3 className="mb-5 text-2xl font-semibold text-center">Messi</h3>
+            <h3 className="mb-5 text-2xl font-semibold text-center">Ronaldo</h3>
+          </div>
+          <div
+            className={`${
+              showMore ? "max-h-auto" : "max-h-[50vh]"
+            } overflow-hidden space-y-5`}
+          >
+            {awardsName.map((award) => (
+              <div key={award}>
+                <p className="font-semibold text-center">{award}</p>
+                <div className="flex">
+                  <div
+                    className={`flex-1 p-3  bg-gradient-to-l
                 ${
                   data.filter(
                     (aw) => aw[type] === award && aw.Player === "Messi"
@@ -77,20 +91,22 @@ export default function Achievements() {
                     ? "from-blue-500"
                     : "from-gray-300"
                 } to-transparent`}
-              >
-                <div className="flex flex-wrap justify-end gap-5 ">
-                  {data
-                    .filter((aw) => aw[type] === award && aw.Player === "Messi")
-                    .sort((a, b) => b.Year - a.Year)
-                    .map((aw, i) => (
-                      <div className="text-sm font-semibold" key={i}>
-                        {aw.Year}
-                      </div>
-                    ))}
-                </div>
-              </div>
-              <div
-                className={`justify-between flex-1 p-3 bg-gradient-to-r 
+                  >
+                    <div className="flex flex-wrap justify-end gap-5 ">
+                      {data
+                        .filter(
+                          (aw) => aw[type] === award && aw.Player === "Messi"
+                        )
+                        .sort((a, b) => b.Year - a.Year)
+                        .map((aw, i) => (
+                          <div className="text-sm font-semibold" key={i}>
+                            {aw.Year}
+                          </div>
+                        ))}
+                    </div>
+                  </div>
+                  <div
+                    className={`justify-between flex-1 p-3 bg-gradient-to-r 
               ${
                 data.filter(
                   (aw) => aw[type] === award && aw.Player === "Ronaldo"
@@ -98,31 +114,79 @@ export default function Achievements() {
                   ? "from-red-500"
                   : "from-gray-300"
               } to-transparent`}
-              >
-                <div className="flex flex-wrap gap-5">
-                  {data
-                    .filter(
-                      (aw) => aw[type] === award && aw.Player === "Ronaldo"
-                    )
-                    .map((aw, i) => (
-                      <div className="text-sm font-semibold" key={i}>
-                        {aw.Year}
-                      </div>
-                    ))}
+                  >
+                    <div className="flex flex-wrap gap-5">
+                      {data
+                        .filter(
+                          (aw) => aw[type] === award && aw.Player === "Ronaldo"
+                        )
+                        .map((aw, i) => (
+                          <div className="text-sm font-semibold" key={i}>
+                            {aw.Year}
+                          </div>
+                        ))}
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <div className="flex justify-center">
-        <button
-          className="px-5 py-3 m-4 text-white rounded shadow bg-emerald-600"
-          onClick={() => setshowMore(!showMore)}
-        >
-          {showMore ? "Show Less" : "Show More"}
-        </button>
-      </div>
+          <div className="flex justify-center">
+            <button
+              className="px-5 py-3 m-4 text-white rounded shadow bg-emerald-600"
+              onClick={() => setshowMore(!showMore)}
+            >
+              {showMore ? "Show Less" : "Show More"}
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div className="flex gap-10">
+          <div className="flex-1 p-5">
+            <h3 className="mb-5 text-2xl font-semibold text-center">Messi</h3>
+            <ul className="space-y-2 list-disc">
+              <li>
+                Cầu thủ trẻ nhất giành Quả bóng Vàng khi mới 23 tuổi vào năm
+                2009
+              </li>
+              <li>
+                Giành được 6 Quả bóng Vàng, trong đó 4 lần liên tiếp vào các năm
+                2009, 2010, 2011 và 2012
+              </li>
+              <li>Ghi nhiều bàn thắng nhất cho một Câu lạc bộ</li>
+              <li>
+                Giành Quả bóng Vàng FIFA, Cầu thủ xuất sắc nhất năm của FIFA,
+                Chiếc giày Vàng châu u và Quả bóng Vàng châu u trong cùng một
+                năm dương lịch
+              </li>
+            </ul>
+          </div>
+          <div className="flex-1 p-5">
+            <h3 className="mb-5 text-2xl font-semibold text-center">Ronaldo</h3>
+            <ul className="space-y-2 list-disc">
+              <li>Cầu thủ ghi bàn nhiều nhất cho 1 đội tuyển quốc gia </li>
+              <li>
+                Vô địch Champions league nhiều nhất thế giới 5 lần(Từ năm 1992
+                đến nay)
+              </li>
+              <li>Vô địch Champions league 3 lần liên tiếp</li>
+              <li>Cầu thủ ghi nhiều bàn nhất lịch sử Champions league</li>
+              <li>Cầu thủ kiến tạo nhiều nhất lịch sử Champions league</li>
+              <li>
+                Cầu thủ ghi nhiều bàn nhất 1 mùa tại Champions league (17 bàn)
+              </li>
+              <li>Ghi bàn 11 trận liên tiếp tại Champions league</li>
+              <li>Ghi bàn tại 3 trận chung kết liên tiếp ở Champions league</li>
+              <li>Ghi bàn nhiều nhất tại Euro </li>
+              <li>Cầu thủ lớn tuổi nhất lập hattrick tại WC</li>
+              <li>
+                Ghi bàn nhiều nhì thế giới (803 bàn, người ghi nhiều nhất là 805
+                bàn)
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/Graph.js
+++ src/Graph.js
@@ -14,19 +14,17 @@ export default function Graph() {
         ></iframe>
         <div className="max-w-4xl mx-auto mb-5 text-justify">
           <p className="mb-2 font-semibold">
-            Số lượng bàn thắng giữa Ronaldo và Messi cấp độ câu lạc
-            bộ
+            Số lượng bàn thắng giữa Ronaldo và Messi cấp độ câu lạc bộ
           </p>
           <p>
-            Đây là chỉ số mà tất cả mọi người đều muốn biết mỗi khi nhắc
-            đến một tiền đạo, mọi người đều phải nghĩ ngay đến, đó là thước đo
-            chính để đánh giá sự thành công, hiệu quả của một tiền đạo ( trừ
-            tiền đạo phòng ngự ). Số lượng bàn thắng của 2 anh trong giai đoạn
-            đỉnh cao thật khủng khiếp khoảng 40-60 bàn 1 mùa, là niềm mơ ước
-            đối với mọi cầu thủ và bất kỳ đội bóng tham vọng nào muốn cạnh tranh danh hiệu cũng thèm thuồng có một tiền
-            đạo như các anh.
+            Đây là chỉ số mà tất cả mọi người đều muốn biết mỗi khi nhắc đến một
+            tiền đạo, mọi người đều phải nghĩ ngay đến, đó là thước đo chính để
+            đánh giá sự thành công, hiệu quả của một tiền đạo ( trừ tiền đạo
+            phòng ngự ). Số lượng bàn thắng của 2 anh trong giai đoạn đỉnh cao
+            thật khủng khiếp khoảng 40-60 bàn 1 mùa, là niềm mơ ước đối với mọi
+            cầu thủ và bất kỳ đội bóng tham vọng nào muốn cạnh tranh danh hiệu
+            cũng thèm thuồng có một tiền đạo như các anh.
             <br /> {/* xuống hàng */}
-            
           </p>
         </div>
         <div className="flex gap-10">
@@ -63,11 +61,14 @@ export default function Graph() {
         ></iframe>
         <div className="max-w-4xl mx-auto mb-10 text-justify">
           <p>
-            Khả năng ghi bàn của CR7 rất đa dạng: gần như các bộ phận trên cơ thể đều có thể ghi bàn, đặc biệt khả năng bật cao để đánh đầu, có thể dứt điểm 2 
-            chân với độ hiểm hóc và lực đi cực tốt. Nếu so với M10 ghi bàn chân trái chủ yếu thì Ronaldo là một tiền đạo toàn diện. Điều này cũng dễ hiểu vì Messi
-            chơi như một số 9 ảo hoặc tiền đạo cánh có xu hướng vẽ vời dắt bóng kiến thiết lối chơi phối hợp với đồng đội còn Ronaldo là một trung phong chọn vị trí 
-            thông minh dứt điểm dứt khoát gọn gàng ngay khi có khoảng trống.
-         
+            Khả năng ghi bàn của CR7 rất đa dạng: gần như các bộ phận trên cơ
+            thể đều có thể ghi bàn, đặc biệt khả năng bật cao để đánh đầu, có
+            thể dứt điểm 2 chân với độ hiểm hóc và lực đi cực tốt. Nếu so với
+            M10 ghi bàn chân trái chủ yếu thì Ronaldo là một tiền đạo toàn diện.
+            Điều này cũng dễ hiểu vì Messi chơi như một số 9 ảo hoặc tiền đạo
+            cánh có xu hướng vẽ vời dắt bóng kiến thiết lối chơi phối hợp với
+            đồng đội còn Ronaldo là một trung phong chọn vị trí thông minh dứt
+            điểm dứt khoát gọn gàng ngay khi có khoảng trống.
           </p>
         </div>
         <Slider />
@@ -80,15 +81,17 @@ export default function Graph() {
           src="graph/goal_equaliser.html"
         ></iframe>
         <div className="max-w-4xl mx-auto mb-10 text-justify">
-          Đội bóng của họ cũng có những lúc gặp khó khăn khi bị đối thủ dẫn trước ở những trận đấu cần điểm để đua top trên bảng xếp hạng hay
-          những trận loại trực tiếp. Vào những thời khắc ngặt nghèo vậy thì các cổ động viên và gần như cả đội bóng đều nhìn vào họ hi vọng 
-          họ sẽ tạo ra sự khác biệt đem lại bàn thắng hay còn gọi là gánh team. Sau những lần như vậy thì chúng ta được nghe điệp khúc "Khi cần Ronaldo/Messi có.
-          Khi khó có Ronaldo/Messi". Đây cũng là một chỉ số cho thấy đẳng cấp của họ. Nhìn lại quá khứ thì có không ít lần họ làm nổ tung cảm xúc của biết bao 
-          người với những bàn thắng xuất thần cứu đội bóng của mình.
-          
-          
+          Đội bóng của họ cũng có những lúc gặp khó khăn khi bị đối thủ dẫn
+          trước ở những trận đấu cần điểm để đua top trên bảng xếp hạng hay
+          những trận loại trực tiếp. Vào những thời khắc ngặt nghèo vậy thì các
+          cổ động viên và gần như cả đội bóng đều nhìn vào họ hi vọng họ sẽ tạo
+          ra sự khác biệt đem lại bàn thắng hay còn gọi là gánh team. Sau những
+          lần như vậy thì chúng ta được nghe điệp khúc "Khi cần Ronaldo/Messi
+          có. Khi khó có Ronaldo/Messi". Đây cũng là một chỉ số cho thấy đẳng
+          cấp của họ. Nhìn lại quá khứ thì có không ít lần họ làm nổ tung cảm
+          xúc của biết bao người với những bàn thắng xuất thần cứu đội bóng của
+          mình.
         </div>
-        ></iframe>
         <div className="flex gap-10">
           <div className="flex-1 w-full h-full p-5 rounded-2xl aspect-video">
             <video
@@ -125,15 +128,20 @@ export default function Graph() {
           src="graph/penalty_pie.html"
         ></iframe>
         <div className="max-w-4xl mx-auto mb-10 text-justify">
-          Mọi người tưởng đá phạt đền rất dễ nhưng sự thật không phải như vậy vì chỉ khi
-          đứng trên chấm 11m chúng ta mới hiểu được áp lực căng thẳng cực kì nặng nề đang bóp nghẹt thở cả người thực 
-          hiện lẫn các cổ dộng viên như thế nào đặc biệt là trong các trận đấu mang tính bước ngoặt vì nếu tạch thì không những làm cho đội
-          nhà chịu các kết quả không như mong muốn, tinh thần xuống dốc mà sẽ trở
-          thành miếng mồi ngon cho các phương tiện truyền thông chế giễu. Chính vì vậy, để ghi bàn từ 1 tình huống phạt đền ngoài
-          kĩ thuật, họ còn phải cần có sự bình tĩnh, lạnh lùng, thần kinh thép cộng thêm một ít tiểu xảo và yếu tố may mắn nữa.
+          Mọi người tưởng đá phạt đền rất dễ nhưng sự thật không phải như vậy vì
+          chỉ khi đứng trên chấm 11m chúng ta mới hiểu được áp lực căng thẳng
+          cực kì nặng nề đang bóp nghẹt thở cả người thực hiện lẫn các cổ dộng
+          viên như thế nào đặc biệt là trong các trận đấu mang tính bước ngoặt
+          vì nếu tạch thì không những làm cho đội nhà chịu các kết quả không như
+          mong muốn, tinh thần xuống dốc mà sẽ trở thành miếng mồi ngon cho các
+          phương tiện truyền thông chế giễu. Chính vì vậy, để ghi bàn từ 1 tình
+          huống phạt đền ngoài kĩ thuật, họ còn phải cần có sự bình tĩnh, lạnh
+          lùng, thần kinh thép cộng thêm một ít tiểu xảo và yếu tố may mắn nữa.
           <br />
-          Tỉ lệ miss pen của M10 có vẻ cao hơn CR7 vì M10 thường hay dùng cái lòng trong chân trái rất ngoan của mình để dứt điểm
-          đánh lừa thủ môn còn CR7 sẽ sút rất mạnh bằng mu vào góc hiểm nên cho dù thủ môn có đoán được hướng cũng khó cản phá được.
+          Tỉ lệ miss pen của M10 có vẻ cao hơn CR7 vì M10 thường hay dùng cái
+          lòng trong chân trái rất ngoan của mình để dứt điểm đánh lừa thủ môn
+          còn CR7 sẽ sút rất mạnh bằng mu vào góc hiểm nên cho dù thủ môn có
+          đoán được hướng cũng khó cản phá được.
         </div>
       </div>
       <div>
@@ -154,8 +162,8 @@ export default function Graph() {
         </div>
       </div>
       <h2 className="pt-8 text-xl font-semibold text-center">
-        Các chỉ số khác để đánh giá tổng quát màn trình diễn đóng góp
-        của mỗi người vào lối chơi chung của toàn đội.
+        Các chỉ số khác để đánh giá tổng quát màn trình diễn đóng góp của mỗi
+        người vào lối chơi chung của toàn đội.
       </h2>
 
       <RadarChart />
